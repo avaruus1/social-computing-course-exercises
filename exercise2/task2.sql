680f54c9686199db47a974aeac44136278d801e3
Ossi Erkkilä <oser0002@gmail.com>
2025-09-29 20:43:31 +0300

    week 2 exercise 2 adjustments

--- exercise2/task2.sql
+++ exercise2/task2.sql
@@ -1,11 +1,11 @@
 SELECT P.id,
        COUNT(DISTINCT C.id)                          comments,
        COUNT(DISTINCT R.id)                          reactions,
-       (COUNT(DISTINCT C.id) + COUNT(DISTINCT R.id)) engagement
+       (COUNT(DISTINCT C.id) + COUNT(DISTINCT R.id)) virality
 FROM users U
          INNER JOIN posts P ON U.id = P.user_id
          LEFT JOIN comments C ON P.id = C.post_id
          LEFT JOIN reactions R ON P.id = R.post_id
 GROUP BY P.id
-ORDER BY engagement DESC
-LIMIT 5;
+ORDER BY virality DESC
+LIMIT 3;
