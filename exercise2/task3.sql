01ac45082c86f33e35915fb846892199491e0ff0
Ossi Erkkilä <oser0002@gmail.com>
2025-09-29 20:49:51 +0300

    week 2 exercise 3: just use days, as the seconds were a bit too large...

--- exercise2/task3.sql
+++ exercise2/task3.sql
@@ -1,5 +1,5 @@
 -- min
-SELECT AVG(ROUND(86400 * (JULIANDAY(C.created_at) - JULIANDAY(P.created_at)))) min_comment_time
+SELECT AVG(JULIANDAY(C.created_at) - JULIANDAY(P.created_at)) min_comment_time
 FROM posts P
          INNER JOIN comments C ON P.id = C.post_id
          INNER JOIN (SELECT IC.post_id pid, MIN(IC.created_at) comment_release_time
@@ -8,7 +8,7 @@ FROM posts P
 WHERE C.created_at = IC.comment_release_time;
 
 -- max
-SELECT AVG(ROUND(86400 * (JULIANDAY(C.created_at) - JULIANDAY(P.created_at)))) max_comment_time
+SELECT AVG(JULIANDAY(C.created_at) - JULIANDAY(P.created_at)) max_comment_time
 FROM posts P
          INNER JOIN comments C ON P.id = C.post_id
          INNER JOIN (SELECT IC.post_id pid, MAX(IC.created_at) comment_release_time
